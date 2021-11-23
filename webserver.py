842db9187612f0f9364e229df085542f25b2808b
Harshavardhini M <harshavardhini332004@gmail.com>
2021-11-23 09:37:56 +0530

    Adding images

--- webserver.py
+++ webserver.py
@@ -1,21 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-To develop a simple webserver to display about top 5 programming languages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
-```
 from http.server import HTTPServer, BaseHTTPRequestHandler
 content = """
 <!DOCTYPE html>
@@ -49,13 +31,3 @@ server_address = ('',8000)
 httpd = HTTPServer(server_address,myhandler)
 print("my webserver is running...")
 httpd.serve_forever()
-
-```
-
-## OUTPUT:
-### CLIENT SIDE OUTPUT
-![Client side](./Clientside.png)
-### SERVER SIDE
-![Server side](./ServerSide.png)
-## RESULT:
-Thus , a simple webserver is designed to display about top 5 programming languages
